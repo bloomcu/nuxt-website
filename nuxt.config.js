779cbadc9bb8bc2c57cdbaa1a66572abb45f30ed
Ryan Harmon <heyharmon@gmail.com>
2022-02-07 13:15:09 -0700

    Use env variable for api base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -76,8 +76,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'https://cms-api.bloomcu.com/api/public/1'
-    // baseURL: 'http://cms.test/api/public/1'
+    baseURL: process.env.BASE_URL,
   },
 
   // Vue Config: https://nuxtjs.org/docs/configuration-glossary/configuration-vue-config/
