cf7f12db8b9bef99a038547aebfab535d1cc363a
Ryan Harmon <heyharmon@gmail.com>
2022-03-02 21:17:47 -0700

    Nuxt link demo

---
+++ pages/a.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container max-width-sm padding-md">
+    <h1>Page A</h1>
+    <br><br>
+    <NuxtLink to="/b">/b</NuxtLink>
+    <br><br>
+    <NuxtLink to="http://localhost:3000/b">http://localhost:3000/b</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  setup(context) {},
+}
+</script>
---
+++ pages/b.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container max-width-sm padding-md">
+    <h1>Page B</h1>
+    <br><br>
+    <NuxtLink to="/b">/b</NuxtLink>
+    <br><br>
+    <NuxtLink to="http://localhost:3000/b">http://localhost:3000/b</NuxtLink>
+    <br><br>
+    <NuxtLink to="https://www.google.com">Google</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  setup(context) {},
+}
+</script>
